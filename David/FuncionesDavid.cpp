c36dcdfd48337bcf74a95361d3ce82f3b0ac36ac
Lopez20005 <bayardo.lopez16083@est.uca.edu.ni>
2023-05-19 20:49:42 -0600

    se agregaron funciones y diferentes para el usuario

---
+++ David/FuncionesDavid.cpp
@@ -0,0 +1,181 @@
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+void calculadora();
+
+int main(){
+	calculadora();
+}
+
+void calculadora() {
+	int cantidad, sentido = 0, viaje = 0;
+	float total;
+	do{
+		cout << "Por favor selecione su ruta " << endl;
+		cout << "1. Nagarote-Managua" << endl;
+		cout << "2. Managua-Nagarote" << endl;
+		cout << "3. Salir" << endl;
+		cout << "Digite la Opcion: " << endl;
+		cin >> sentido;
+
+		switch (sentido){
+			case 1:
+				do{
+					cout << "Cual es el Destino?" << endl;
+					cout << "1. Nagarote - Managua" << endl;
+					cout << "2. Nagarote - Cementera Holcim" << endl;
+					cout << "3. Nagarote - Zona Franca" << endl;
+					cout << "4. Mateare - Zona Franca" << endl;
+					cout << "5. Mateare - Managua" << endl;
+					cout << "6. Ciudad Sandino - Managua" << endl;
+					cout << "7. Origen y Destino Variable" << endl;
+					cout << "8. Salir de este menu" <<endl;
+					cout << "Digite el destino como se indica en las opciones: " << endl;
+					cin >> viaje;
+				
+					switch(viaje) {
+						case 1:
+							float costoPasaje;
+							cout<<"Ingrese el precio actual del pasaje: ";
+							cin>> costoPasaje;
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*costoPasaje;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 2:
+							
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*10.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 3:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*16.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 4:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*10.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 5:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*13.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 6:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*8.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 7:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*20.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						default:
+							cout<<"Opción incorrecta, vuelve a intentar"<<endl;
+							system("pause");
+							break;
+					}
+				}while(viaje!=8);
+				break;
+
+			case 2:
+				do{
+					cout << "Cual es el Destino?" << endl;
+					cout << "1. Managua - Nagarote" << endl;
+					cout << "2. Managua - Mateare" << endl;
+					cout << "3. Managua - Km21" << endl;
+					cout << "4. Managua - Ciudad Doral" << endl;
+					cout << "5. Managua - Los Brasiles" << endl;
+					cout << "6. Ciudad Sandino - Mateare" << endl;
+					cout << "7. Origen y Destino Variable" << endl;
+					cout << "8. Salir de este menu" <<endl;
+					cout << "Digite el destino como se indica en las opciones: " << endl;
+					cin >> viaje;
+					switch (viaje) {
+						case 1:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*23.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 2:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*13.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 3:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*12.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 4:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*10.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 5:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*9.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						case 6:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*8.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause"); 
+							break;
+						case 7:
+							cout << "Dime la cantidad de Pasajes: " << endl;
+							cin >> cantidad;
+							total = cantidad*20.00;
+							cout << "El precio total sera de " << total << endl;
+							system("pause");
+							break;
+						default:
+							cout<<"Opción incorrecta, vuelve a intentar"<<endl;
+							system("pause");
+							break;
+					}
+					
+				}while(viaje !=8);
+				break;
+				
+			case 3:
+				cout<<"Adios"<<endl;
+				break;
+			
+			default:
+				cout<<"Opción incorrecta, vuelve a intentar"<<endl;
+				system("pause");
+				break;
+		}
+	}while(sentido != 3);
+}
