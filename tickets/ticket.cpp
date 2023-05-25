8834c928ea92ab7bb234ea059aca8fe8943fbda7
Lopez20005 <bayardo.lopez16083@est.uca.edu.ni>
2023-05-24 23:41:40 -0600

    Modificamos el archivo

--- tickets/ticket.cpp
+++ tickets/ticket.cpp
@@ -1,214 +1,144 @@
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "../auxiliar.h"
+
+#define MAX 14
 using namespace std;
-typedef struct 
+typedef struct
 {
-	int id;
+	char id[5];
 	char origen[50];
 	char destino[50];
 	float price;
 } priceTicket;
 typedef struct
 {
-    int day;
-    int month;
-    int year;
+	int day;
+	int month;
+	int year;
 } date;
 typedef struct
 {
- int idF;
- date dateF;
- int idhorario;
- int cantT;
- float price;
- priceTicket ticket;
-}factura;
+	char idF[5];
+	date dateF;
+	int horario;
+	int cantT;
+	priceTicket ticket;
+	
+	
+} factura;
 
-priceTicket pt[14];
+
+factura Fact[500];
+priceTicket PT[MAX];
+int lastRegDestiny = 0;
+int lastRegFact = 0 ; 
+
+/* declaramos las siguientes Funciones
+1. add destiny
+2. edit destiny
+3. search destiny
+4. print Factura
+5. mostar monto del dia
+6. mostrar monto por fecha*/
+
+void addDestiny(priceTicket destiny);
+void editDestiny();
+int searchDestiny(char id[]);
+void addFactura (factura F);
+int searchFactura(char id[]);
+void printFactura(int pos, int pos1,float price);
+void showDay(int pos);
+void showDate(date thisDate);
+
+
+void addDestiny(priceTicket destiny)
+{
+	PT[lastRegDestiny] = destiny;
+	lastRegDestiny++;
+}
+
+void editDestiny(priceTicket destiny, int pos)
+{
+	PT[pos] = destiny;
+}
+
+priceTicket getTicket(int pos)
+{
+    return PT[pos];
+}
+
+int searchDestiny(char num[])
+{
+    int posicion = 0;
+    for (int i = 0; i < lastRegDestiny; i++)
+    {
+        if (strcmp(num, PT[i].id) == 0)
+        {
+            posicion = i;
+            break;
+        }
+    }
+    return posicion;
+}
+
+void showDay(int pos)
+{
+    gotoxy(60, 5);
+    
+}
+
+void showDate()
+{
  
- /*Funcioces
- 1. add destiny
- 2. edit destiny
- 3. search destiny
- 4. add Factura
- 5. mostar monto del dia
- 6. mostrar monto por fecha*/
-
-void calculadora();
-
-int main(){
-	calculadora();
 }
 
-void calculadora() {
-	int cantidad, sentido = 0, viaje = 0;
-	float total;
-	do{
-		cout << "Asignar pasajes\n";
-		cout << "Por favor selecione su ruta " << endl;
-		cout << "1. Nagarote-Managua" << endl;
-		cout << "2. Managua-Nagarote" << endl;
-		cout << "3. Salir" << endl;
-		cout << "Digite la Opcion: " << endl;
-		cin >> sentido;
+void addFactura(factura F)
+{
+Fact[lastRegFact] = F ;
+	lastRegFact++;
 
-		switch (sentido){
-			case 1:
-				do{
-					cout << "Cual es el Destino?" << endl;
-					cout << "1. Nagarote - Managua" << endl;
-					cout << "2. Nagarote - Cementera Holcim" << endl;
-					cout << "3. Nagarote - Zona Franca" << endl;
-					cout << "4. Mateare - Zona Franca" << endl;
-					cout << "5. Mateare - Managua" << endl;
-					cout << "6. Ciudad Sandino - Managua" << endl;
-					cout << "7. Origen y Destino Variable" << endl;
-					cout << "8. Salir de este menu" <<endl;
-					cout << "Digite el destino como se indica en las opciones: " << endl;
-					cin >> viaje;
-				
-					switch(viaje) {
-						case 1:
-							float costoPasaje;
-							cout<<"Ingrese el precio actual del pasaje: ";
-							cin>> costoPasaje;
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*costoPasaje;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 2:
-							
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*10.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 3:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*16.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 4:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*10.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 5:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*13.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 6:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*8.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 7:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*20.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						default:
-							cout<<"Opción incorrecta, vuelve a intentar"<<endl;
-							system("pause");
-							break;
-					}
-				}while(viaje!=8);
-				break;
-
-			case 2:
-				do{
-					cout << "Cual es el Destino?" << endl;
-					cout << "1. Managua - Nagarote" << endl;
-					cout << "2. Managua - Mateare" << endl;
-					cout << "3. Managua - Km21" << endl;
-					cout << "4. Managua - Ciudad Doral" << endl;
-					cout << "5. Managua - Los Brasiles" << endl;
-					cout << "6. Ciudad Sandino - Mateare" << endl;
-					cout << "7. Origen y Destino Variable" << endl;
-					cout << "8. Salir de este menu" <<endl;
-					cout << "Digite el destino como se indica en las opciones: " << endl;
-					cin >> viaje;
-					switch (viaje) {
-						case 1:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*23.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 2:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*13.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 3:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*12.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 4:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*10.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 5:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*9.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						case 6:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*8.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause"); 
-							break;
-						case 7:
-							cout << "Dime la cantidad de Pasajes: " << endl;
-							cin >> cantidad;
-							total = cantidad*20.00;
-							cout << "El precio total sera de " << total << endl;
-							system("pause");
-							break;
-						default:
-							cout<<"Opción incorrecta, vuelve a intentar"<<endl;
-							system("pause");
-							break;
-					}
-					
-				}while(viaje !=8);
-				break;
-				
-			case 3:
-				cout<<"Adios"<<endl;
-				break;
-			
-			default:
-				cout<<"Opción incorrecta, vuelve a intentar"<<endl;
-				system("pause");
-				break;
-		}
-	}while(sentido != 3);
 }
+int searchFactura(char id[]){
+
+int posicion = 0;
+    for (int i = 0; i < lastRegFact; i++)
+    {
+        if (strcmp(id, Fact[i].idF) == 0)
+        {
+            posicion = i;
+            break;
+        }
+    }
+    return posicion;
+
+
+
+}
+void printFactura(int pos, int pos1,float price)
+{
+ gotoxy(60,5);
+    cout << " ID Factura " << Fact[pos].idF << endl;
+    gotoxy(60,6);
+    cout << "Fecha: " << Fact[pos].dateF.day << "/" << Fact[pos].dateF.month << "/" << Fact[pos].dateF.year << endl;
+    gotoxy(60,7); 
+    cout << " Horario: " << Fact[pos].horario<< endl;
+    gotoxy(60,8);
+    cout << "Cantidad de  " << Fact[pos].cantT << endl;
+    gotoxy(60,9);
+    cout << "ID destino " << Fact[pos1].ticket.id << endl ; 
+    gotoxy(60,10);
+    cout << "Origen:" << Fact[pos1].ticket.origen << endl ; 
+	 gotoxy(60,11);
+    cout << "Destino: " << Fact[pos1].ticket.destino<< endl;
+    gotoxy(60,12);
+    cout << "Precio individual:  " << Fact[pos1].ticket.price<< endl;
+    gotoxy(60,13);
+    cout << "Total:" << price = Fact[pos1].ticket.price * Fact[pos].cantT;
+}
+
+
+
+
